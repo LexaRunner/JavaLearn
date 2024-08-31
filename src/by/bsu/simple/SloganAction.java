80ed18372e70720782be1a3c09812b52a0a24b4a
Aleksey Zashikhin <leha.polihim@gmail.com>
2024-08-31 16:11:56 +0500

    SloganAction

---
+++ src/by/bsu/simple/SloganAction.java
@@ -0,0 +1,14 @@
+package by.bsu.simple;
+
+public class SloganAction {
+    public static void main(String[] args) {
+        ActionSlogan firstObject = new ActionSlogan();
+        firstObject.ptintSlogan();
+    }
+}
+
+class ActionSlogan {
+    void ptintSlogan() {
+        System.out.println("Enabling the information age");
+    }
+}
